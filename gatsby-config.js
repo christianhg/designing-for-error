d396c1c95314e73d7370b1a264c303b186ebb7e5
Christian Hamburger Grøngaard <christianhg@gmail.com>
2019-04-11 21:14:06 +0200

    Remove gatsby-remark-copy-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          `gatsby-remark-copy-images`,
           {
             resolve: `gatsby-remark-images`,
             options: {
